97503db9bfbca8caab462738645ebf63e04aef76
Ice Lee <lb291700351@live.cn>
2018-07-24 15:57:00 +0800

    Add docker auto build and run script

---
+++ docker-run.bat
@@ -0,0 +1,4 @@
+gradlew.bat clean \
+:manager-zuul:copyDocker \
+:eureka-register-center:copyDocker \
+:manager-server-api-admin:copyDocker
--- manager-server-api/src/main/kotlin/com/github/lee/server/api/AdminServerAPI.kt
+++ manager-server-api/src/main/kotlin/com/github/lee/server/api/AdminServerAPI.kt
@@ -3,10 +3,8 @@ package com.github.lee.server.api
 import com.github.lee.core.controller.domain.JsonResponse
 import org.springframework.web.bind.annotation.RequestMapping
 
-@RequestMapping(AdminServerAPI.MAPPING_ROOT)
 interface AdminServerAPI {
     companion object {
-        const val MAPPING_ROOT = "/admin"
         private const val MAPPING_TEST = "/test"
     }
 
